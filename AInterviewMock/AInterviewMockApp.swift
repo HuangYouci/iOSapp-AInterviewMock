0de786f6e407986b7d3251f4546e0dd769801375
Hunag Youci <huangyouci@icloud.com>
2025-05-04 17:54:25 +0800

    Initial Commit

---
+++ AInterviewMock/AInterviewMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AInterviewMockApp.swift
+//  AInterviewMock
+//
+//  Created by 黃宥琦 on 2025/5/4.
+//
+
+import SwiftUI
+
+@main
+struct AInterviewMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AInterviewMock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AInterviewMock
+//
+//  Created by 黃宥琦 on 2025/5/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
